06595f8c4ba70414e70f7bafaab850e2c4b40c2e
KCY <ghkdua059@gmail.com>
2020-05-01 20:11:48 +0900

    homeActivity 및 이미지 추가
    Fragment 사용 예정

---
+++ app/src/main/java/com/earlyBuddy/earlybuddy_android/ui/home/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.earlyBuddy.earlybuddy_android.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.earlyBuddy.earlybuddy_android.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+
+    }
+}
