9e9e0ee8c255af03c4ffa06ddef5b17e45e0ed2b
KCY <ghkdua059@gmail.com>
2020-05-03 19:11:24 +0900

    [홈뷰] 프래그먼트 생성
    1. 스케쥴 없을때 2. 스케줄 있지만 여유있을때
    3. 버스 3,2,1대 전

---
+++ app/src/main/java/com/earlyBuddy/earlybuddy_android/ui/home/BeforeDayFragment/BeforeDayFragment.kt
@@ -0,0 +1,22 @@
+package com.earlyBuddy.earlybuddy_android.ui.home.BeforeDayFragment
+
+import android.os.Bundle
+import com.earlyBuddy.earlybuddy_android.R
+import com.earlyBuddy.earlybuddy_android.base.BaseFragment
+import com.earlyBuddy.earlybuddy_android.databinding.FragmentHomeBeforeDayBinding
+
+class BeforeDayFragment : BaseFragment<FragmentHomeBeforeDayBinding, BeforeDayViewModel>() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+
+    override val layoutResID: Int
+        get() = R.layout.fragment_home_before_day
+    override val viewModel: BeforeDayViewModel
+        get() = BeforeDayViewModel()
+}
--- app/src/main/java/com/earlyBuddy/earlybuddy_android/ui/home/NoScheduleFragment/NoScheduleFragment.kt
+++ app/src/main/java/com/earlyBuddy/earlybuddy_android/ui/home/NoScheduleFragment/NoScheduleFragment.kt
@@ -1,13 +1,14 @@
-package com.earlyBuddy.earlybuddy_android.ui.home
+package com.earlyBuddy.earlybuddy_android.ui.home.NoScheduleFragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.earlyBuddy.earlybuddy_android.R
+import com.earlyBuddy.earlybuddy_android.base.BaseFragment
+import com.earlyBuddy.earlybuddy_android.databinding.FragmentHomeNoScheduleBinding
 
-class NoPlanFragment : Fragment() {
+class NoScheduleFragment : BaseFragment<FragmentHomeNoScheduleBinding, NoScheduleViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -20,5 +21,10 @@ class NoPlanFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_home_no_schedule,null)
         return view
     }
+
+    override val layoutResID: Int
+        get() = R.layout.fragment_home_no_schedule
+    override val viewModel: NoScheduleViewModel
+        get() = NoScheduleViewModel()
 
 }
