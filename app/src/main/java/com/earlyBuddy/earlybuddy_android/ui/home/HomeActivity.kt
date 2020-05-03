8b4b06662f88c8cf70a2c908049c9b910732a2bf
KCY <ghkdua059@gmail.com>
2020-05-03 19:10:35 +0900

    [홈뷰] 홈액티비티 베이스액티비티 상속

--- app/src/main/java/com/earlyBuddy/earlybuddy_android/ui/home/HomeActivity.kt
+++ app/src/main/java/com/earlyBuddy/earlybuddy_android/ui/home/HomeActivity.kt
@@ -1,23 +1,34 @@
 package com.earlyBuddy.earlybuddy_android.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.earlyBuddy.earlybuddy_android.R
+import com.earlyBuddy.earlybuddy_android.base.BaseActivity
+import com.earlyBuddy.earlybuddy_android.databinding.ActivityHomeBinding
+import com.earlyBuddy.earlybuddy_android.ui.home.BeforeBusFragment.BeforeBusFragment
+import com.earlyBuddy.earlybuddy_android.ui.home.BeforeDayFragment.BeforeDayFragment
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BaseActivity<ActivityHomeBinding, HomeViewModel>() {
+
+    override val layoutResID: Int
+        get() = R.layout.activity_home
+    override val viewModel: HomeViewModel
+        get() = HomeViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
 
-        var random =( Math.random()*2).toInt()
+        var random = (Math.random() * 2).toInt()
         if(random ==1) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.act_home_fl_home_fragment, BeforeDayFragment())
+                .replace(R.id.act_home_fl_home_fragment,
+                    BeforeBusFragment()
+                )
                 .commit()
         }else{
             supportFragmentManager.beginTransaction()
-                .replace(R.id.act_home_fl_home_fragment, NoPlanFragment())
+                .replace(R.id.act_home_fl_home_fragment,
+                    BeforeDayFragment()
+                )
                 .commit()
         }
     }
