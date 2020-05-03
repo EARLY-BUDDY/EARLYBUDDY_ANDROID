b1da2f7b4eb2a6cbf649e87c3953da1f6b86104d
KCY <ghkdua059@gmail.com>
2020-05-03 19:12:47 +0900

    [홈뷰] 홈뷰 통신 추가

--- app/src/main/java/com/earlyBuddy/earlybuddy_android/data/datasource/remote/retrofit/NetworkService.kt
+++ app/src/main/java/com/earlyBuddy/earlybuddy_android/data/datasource/remote/retrofit/NetworkService.kt
@@ -1,8 +1,13 @@
 package com.example.android_study_favorite.datasource.remote.retrofit
 
+import com.earlyBuddy.earlybuddy_android.data.datasource.model.HomeResponse
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NetworkService {
+    @GET("/searchAddress")
+    fun getAdress(
+        @Query("addr") addr: String
+    ): Observable<HomeResponse>
 }
