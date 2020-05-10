8ec0e492fd318bdced3c78c27d8f61d23902e4a8
yejin <jinee0717@daum.net>
2020-05-11 00:25:47 +0900

    [로그인] 로그인 뷰 작동 완료

---
+++ app/src/main/java/com/earlyBuddy/earlybuddy_android/ui/signUp/SignInViewModel.kt
@@ -0,0 +1,8 @@
+package com.earlyBuddy.earlybuddy_android.ui.signUp
+
+import com.earlyBuddy.earlybuddy_android.base.BaseViewModel
+import com.earlyBuddy.earlybuddy_android.data.repository.SignInRepository
+
+class SignInViewModel(repository: SignInRepository) : BaseViewModel(){
+
+}
