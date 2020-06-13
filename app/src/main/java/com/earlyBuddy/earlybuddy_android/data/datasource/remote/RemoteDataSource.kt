258825c4348d2b00a75b976db88acdb95f935a74
KCY <ghkdua059@gmail.com>
2020-06-13 18:23:09 +0900

    [세로경로] 경로찾기 rest 데이터 받기

--- app/src/main/java/com/earlyBuddy/earlybuddy_android/data/datasource/remote/RemoteDataSource.kt
+++ app/src/main/java/com/earlyBuddy/earlybuddy_android/data/datasource/remote/RemoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.earlyBuddy.earlybuddy_android.data.datasource.remote
 
 import com.earlyBuddy.earlybuddy_android.data.datasource.model.PlaceResponse
+import com.earlyBuddy.earlybuddy_android.data.datasource.model.SearchRouteResponse
 import com.earlyBuddy.earlybuddy_android.data.datasource.model.SignInResponse
 import com.earlyBuddy.earlybuddy_android.data.datasource.model.SignUpResponse
 import com.google.gson.JsonObject
@@ -10,4 +11,11 @@ interface RemoteDataSource {
     fun searchPlace(query: String) : Observable<PlaceResponse>
     fun signUp(jsonObject: JsonObject) : Observable<SignUpResponse>
     fun signIn(jsonObject: JsonObject) : Observable<SignInResponse>
+    fun searchRoute(
+        SX: Double,
+        SY: Double,
+        EX: Double,
+        EY: Double,
+        SearchPathType: Int
+    ): Observable<SearchRouteResponse>
 }
--- app/src/main/java/com/earlyBuddy/earlybuddy_android/data/repository/SearchRouteRepository.kt
+++ app/src/main/java/com/earlyBuddy/earlybuddy_android/data/repository/SearchRouteRepository.kt
@@ -1,4 +1,13 @@
 package com.earlyBuddy.earlybuddy_android.data.repository
 
-class SearchRouteRepository {
+import com.earlyBuddy.earlybuddy_android.data.datasource.remote.RemoteDataSource
+
+class SearchRouteRepository(private val remoteDataSource: RemoteDataSource) {
+    fun getSearchRouteData(
+        SX: Double,
+        SY: Double,
+        EX: Double,
+        EY: Double,
+        SearchPathType: Int
+    ) = remoteDataSource.searchRoute(SX, SY, EX, EY, SearchPathType)
 }
