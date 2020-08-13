de7c17bcfb7d2841ef8d73a9a179d209b152444c
KCY <ghkdua059@gmail.com>
2020-08-13 22:53:21 +0900

    [의존성주입] 앱모듈 수정

--- app/src/main/java/com/earlyBuddy/earlybuddy_android/data/repository/InitialPlaceRepository.kt
+++ app/src/main/java/com/earlyBuddy/earlybuddy_android/data/repository/InitialPlaceRepository.kt
@@ -1,2 +1,7 @@
 package com.earlyBuddy.earlybuddy_android.data.repository
 
+import com.earlyBuddy.earlybuddy_android.data.datasource.remote.RemoteDataSource
+
+class InitialPlaceRepository (private val remoteDataSource: RemoteDataSource){
+
+}
--- app/src/main/java/com/earlyBuddy/earlybuddy_android/di/ViewModelAppModule.kt
+++ app/src/main/java/com/earlyBuddy/earlybuddy_android/di/ViewModelAppModule.kt
@@ -4,6 +4,7 @@ import com.earlyBuddy.earlybuddy_android.ui.home.HomeViewModel
 import com.earlyBuddy.earlybuddy_android.ui.home.beforeBus.BeforeBusViewModel
 import com.earlyBuddy.earlybuddy_android.ui.home.beforeDay.BeforeDayViewModel
 import com.earlyBuddy.earlybuddy_android.ui.home.pathCheck.HomePathViewModel
+import com.earlyBuddy.earlybuddy_android.ui.initial.place.InitialPlaceViewModel
 import com.earlyBuddy.earlybuddy_android.ui.pathSearch.PathViewModel
 import com.earlyBuddy.earlybuddy_android.ui.placeSearch.PlaceSearchViewModel
 import com.earlyBuddy.earlybuddy_android.ui.signUp.SignInViewModel
@@ -20,4 +21,5 @@ val viewModelAppModule = module {
     viewModel { HomePathViewModel(get()) }
     viewModel { BeforeBusViewModel() }
     viewModel { BeforeDayViewModel() }
+    viewModel { InitialPlaceViewModel(get()) }
 }
