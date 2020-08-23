b828e5116592a1e21b523e76f7b792d35bcb4916
yejin <jinee0717@daum.net>
2020-08-24 02:06:32 +0900

    [경로검색] 가로경로 반올림으로 수정

--- app/src/main/java/com/earlyBuddy/earlybuddy_android/ui/pathSearch/PathMethodAdapter.kt
+++ app/src/main/java/com/earlyBuddy/earlybuddy_android/ui/pathSearch/PathMethodAdapter.kt
@@ -9,6 +9,7 @@ import com.earlyBuddy.earlybuddy_android.R
 import com.earlyBuddy.earlybuddy_android.TransportMap
 import com.earlyBuddy.earlybuddy_android.data.datasource.model.SubPath
 import com.earlyBuddy.earlybuddy_android.databinding.ItemPathMethodBinding
+import kotlin.math.round
 
 class PathMethodAdapter : RecyclerView.Adapter<PathMethodViewHolder>() {
 
@@ -41,12 +42,12 @@ class PathMethodAdapter : RecyclerView.Adapter<PathMethodViewHolder>() {
 
 class PathMethodViewHolder(val binding: ItemPathMethodBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(totalLen : Int, totalTime : Int, minWalkLen : Int, minTransLen : Int, data : SubPath, trafficType: Int, flag : Boolean){
-        var methodLen = 0f
-        if(data.sectionTime!=0) methodLen = totalLen * (data.sectionTime.toFloat() / totalTime.toFloat())
-//        Log.e("PathMethodViewHolder@@@@@@@@@ totalLen", totalLen.toString())
-//        Log.e("PathMethodViewHolder@@@@@@@@@ minLen", "${minWalkLen} + ${minTransLen}")
-//        Log.e("PathMethodViewHolder@@@@@@@@@ totalTime", totalTime.toString())
-//        Log.e("PathMethodViewHolder@@@@@@@@@ sectionTime", data.sectionTime.toString())
+        var methodLen = 0
+        if(data.sectionTime!=0) methodLen = round(totalLen * (data.sectionTime.toFloat() / totalTime.toFloat())).toInt()
+        Log.e("PathMethodViewHolder@@@@@@@@@ totalLen", totalLen.toString())
+        Log.e("PathMethodViewHolder@@@@@@@@@ minLen", "${minWalkLen} + ${minTransLen}")
+        Log.e("PathMethodViewHolder@@@@@@@@@ totalTime", totalTime.toString())
+        Log.e("PathMethodViewHolder@@@@@@@@@ sectionTime", data.sectionTime.toString())
         Log.e("PathMethodViewHolder@@@@@@@@@ methodLen", methodLen.toString())
 
         binding.trafficType = data.trafficType
@@ -54,21 +55,21 @@ class PathMethodViewHolder(val binding: ItemPathMethodBinding) : RecyclerView.Vi
         if (flag){
             binding.methodName = "${data.sectionTime}분"
             binding.methodColor = "1"
-            binding.itemPathMethodRl.layoutParams.width = methodLen.toInt() + minWalkLen
+            binding.itemPathMethodRl.layoutParams.width = methodLen + minWalkLen
         }
         else{
             if(trafficType == 3){
                 if(data.sectionTime==0) {
-                    binding.itemPathMethodRl.layoutParams.width = methodLen.toInt()
+                    binding.itemPathMethodRl.layoutParams.width = methodLen
                     binding.methodName = " "
                     binding.methodColor = "0"
                 } else {
-                    binding.itemPathMethodRl.layoutParams.width = methodLen.toInt() + minWalkLen
+                    binding.itemPathMethodRl.layoutParams.width = methodLen + minWalkLen
                     binding.methodName = "${data.sectionTime}분"
                     binding.methodColor = "1"
                 }
             }else{
-                binding.itemPathMethodRl.layoutParams.width = methodLen.toInt() + minTransLen
+                binding.itemPathMethodRl.layoutParams.width = methodLen + minTransLen
                 if(trafficType == 1){
                     binding.methodColor = TransportMap.subwayMap[data.lane!!.type]!![0]
                     binding.methodName = TransportMap.subwayMap[data.lane.type]!![1]
