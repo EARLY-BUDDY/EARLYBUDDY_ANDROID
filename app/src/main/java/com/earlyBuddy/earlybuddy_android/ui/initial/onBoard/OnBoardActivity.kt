a2fab0c71c136ececb37d3d38d258d50d941345c
KCY <ghkdua059@gmail.com>
2020-11-16 22:02:31 +0900

    [온보딩] 온보딩 완성

---
+++ app/src/main/java/com/earlyBuddy/earlybuddy_android/ui/initial/onBoard/OnBoardActivity.kt
@@ -0,0 +1,11 @@
+package com.earlyBuddy.earlybuddy_android.ui.initial.onBoard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_board)
+    }
+}
